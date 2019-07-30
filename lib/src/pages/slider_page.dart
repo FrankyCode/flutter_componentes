5bb75a1ac692cce79c738df56b252233de6eb7e5
Franky Coding <fran-15-16@hotmail.com>
2019-07-30 10:34:01 +0100

    07-slider

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -6,10 +6,48 @@ class SliderPage extends StatefulWidget {
 }
 
 class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sliders'),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Column(
+          children: <Widget>[
+            _crearSlide(),
+            Expanded(child: _crearImagen()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _crearSlide() {
+    return Slider(
+      label: 'Tamaño de la Imagen',    // Mensaje al mover el slide
+    //  divisions: 20,                // Segmentacion del Slide cada 20 puntos
+      value: _valorSlider,            // valor inicial por el que comienza el SLide
+      min: 10.0,                      // valor minimo del SLide
+      max: 400.0,                     // valor maximo del Slide
+      onChanged: (valor) {
+        setState(() {
+          _valorSlider = valor;
+          print(valor);
+        });
+      },
+    );
+  }
+
+  Widget _crearImagen() {
+    return Image(
+      image:
+          NetworkImage('https://i.ytimg.com/vi/fq4N0hgOWzU/maxresdefault.jpg'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
     );
   }
 }
--- lib/src/utils/icono_string_util.dart
+++ lib/src/utils/icono_string_util.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 final _icons = <String, IconData>{
-  'add_alert': Icons.add_alert,
+  'add_alert'    : Icons.add_alert,
   'accessibility': Icons.accessibility,
-  'folder_open': Icons.folder_open,
-  'donut_large' : Icons.donut_large,
-  'input' : Icons.input,
-  'list' : Icons.list
+  'folder_open'  : Icons.folder_open,
+  'donut_large'  : Icons.donut_large,
+  'input'        : Icons.input,
+  'tune'         : Icons.tune,
+  'list'         : Icons.list,
 };
 
 Icon getIcon(String nombreIcono) {
